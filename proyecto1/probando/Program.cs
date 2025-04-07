f73aaad8186a90207027fceb98129d9682844c1c
rudi Intor <rudiintor123@gmail.com>
2025-04-06 21:58:48 -0500

    codena nueva a

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -2,3 +2,6 @@
 Console.WriteLine("Hello, Platzi!");
 
 string stripe api key= sk_test_4eC39HqLyjWDarjtT1zdp7dc
+
+
+steing testingConnection= "Server=tcp:aminesserver.database.windows.net,1433;Initial Catalog=amine;Persist Security Info=False;User ID=amine;Password=1234567890;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
